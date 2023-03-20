88874631eda6908787f304dd570d985ffc4ddd0f
Kerim Amansaryyev <keamansaryyev@gmail.com>
2023-03-20 23:47:55 +0500

    Added option to enable the pinch to zoom feature

--- lib/src/core/better_player_with_controls.dart
+++ lib/src/core/better_player_with_controls.dart
@@ -101,40 +101,47 @@ class _BetterPlayerWithControlsState extends State<BetterPlayerWithControls> {
     }
 
     aspectRatio ??= 16 / 9;
+    final bool isPinchToZoomEnabled =
+        betterPlayerController.betterPlayerConfiguration.enablePinchToZoom;
+
     final innerContainer = Container(
       width: double.infinity,
       color: betterPlayerController
           .betterPlayerConfiguration.controlsConfiguration.backgroundColor,
       child: GestureDetector(
-        onScaleUpdate: (details) {
-          // calculating difference between new scale and previous value
-          // when zooming out, a factor of 2 is used so that the zoom value can be reduced to 0
-          // when zooming in, a factor of 2 is used to uniformly change the zoom value
-          double diff = (details.scale - scaleListener.value) * 2;
+        onScaleUpdate: !isPinchToZoomEnabled
+            ? null
+            : (details) {
+                // calculating difference between new scale and previous value
+                // when zooming out, a factor of 2 is used so that the zoom value can be reduced to 0
+                // when zooming in, a factor of 2 is used to uniformly change the zoom value
+                double diff = (details.scale - scaleListener.value) * 2;
 
-          // checking current zoom for maximum and minimum value
-          // minimum value is 0 - video not showing on notch area
-          // maximum value is 1 - video showing on notch area
-          if (zoomListener.value + diff > 1) {
-            zoomListener.value = 1;
-          } else if (zoomListener.value + diff < 0) {
-            zoomListener.value = 0;
-          } else {
-            zoomListener.value += diff;
-          }
-          // set current scale as scale value
-          scaleListener.value = details.scale;
-        },
-        onScaleEnd: (details) {
-          // bringing zoom value to maximum or minimum
-          if (zoomListener.value <= 0.5) {
-            zoomListener.value = 0;
-          } else {
-            zoomListener.value = 1;
-          }
-          // set default scale value
-          scaleListener.value = 1;
-        },
+                // checking current zoom for maximum and minimum value
+                // minimum value is 0 - video not showing on notch area
+                // maximum value is 1 - video showing on notch area
+                if (zoomListener.value + diff > 1) {
+                  zoomListener.value = 1;
+                } else if (zoomListener.value + diff < 0) {
+                  zoomListener.value = 0;
+                } else {
+                  zoomListener.value += diff;
+                }
+                // set current scale as scale value
+                scaleListener.value = details.scale;
+              },
+        onScaleEnd: !isPinchToZoomEnabled
+            ? null
+            : (details) {
+                // bringing zoom value to maximum or minimum
+                if (zoomListener.value <= 0.5) {
+                  zoomListener.value = 0;
+                } else {
+                  zoomListener.value = 1;
+                }
+                // set default scale value
+                scaleListener.value = 1;
+              },
         child: AspectRatio(
           aspectRatio: aspectRatio,
           child: _buildPlayerWithControls(betterPlayerController, context),
@@ -165,26 +172,33 @@ class _BetterPlayerWithControlsState extends State<BetterPlayerWithControls> {
 
     final bool placeholderOnTop =
         betterPlayerController.betterPlayerConfiguration.placeholderOnTop;
+    final bool isPinchToZoomEnabled =
+        betterPlayerController.betterPlayerConfiguration.enablePinchToZoom;
+    final betterPlayerVideoFitWidget = Transform.rotate(
+      angle: rotation * pi / 180,
+      child: _BetterPlayerVideoFitWidget(
+        betterPlayerController,
+        betterPlayerController.getFit(),
+      ),
+    );
+
     // ignore: avoid_unnecessary_containers
     return Container(
       child: Stack(
         fit: StackFit.passthrough,
         children: <Widget>[
           if (placeholderOnTop) _buildPlaceholder(betterPlayerController),
-          AnimatedBuilder(
-            animation: zoomListener,
-            builder: (context, child) => Padding(
-              padding: EdgeInsets.zero +
-                  MediaQuery.of(context).padding * (1 - zoomListener.value),
-              child: Transform.rotate(
-                angle: rotation * pi / 180,
-                child: _BetterPlayerVideoFitWidget(
-                  betterPlayerController,
-                  betterPlayerController.getFit(),
-                ),
+          if (isPinchToZoomEnabled)
+            AnimatedBuilder(
+              animation: zoomListener,
+              builder: (context, child) => Padding(
+                padding: EdgeInsets.zero +
+                    MediaQuery.of(context).padding * (1 - zoomListener.value),
+                child: betterPlayerVideoFitWidget,
               ),
-            ),
-          ),
+            )
+          else
+            betterPlayerVideoFitWidget,
           betterPlayerController.betterPlayerConfiguration.overlay ??
               Container(),
           BetterPlayerSubtitlesDrawer(
