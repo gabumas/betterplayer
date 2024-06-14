d5ad1607ed52887a12ed1f7ab9e3779146c60b43
Alexander Korobkin <alexanderkorobkin.ak@gmail.com>
2024-06-14 12:57:21 +0200

    Fixed deprecated usage of Theme properties

--- lib/src/configuration/better_player_controls_configuration.dart
+++ lib/src/configuration/better_player_controls_configuration.dart
@@ -260,8 +260,8 @@ class BetterPlayerControlsConfiguration {
   ///Setup BetterPlayerControlsConfiguration based on Theme options.
   factory BetterPlayerControlsConfiguration.theme(ThemeData theme) {
     return BetterPlayerControlsConfiguration(
-      textColor: theme.textTheme.bodyText1?.color ?? Colors.white,
-      iconsColor: theme.textTheme.button?.color ?? Colors.white,
+      textColor: theme.textTheme.bodyLarge?.color ?? Colors.white,
+      iconsColor: theme.textTheme.displayLarge?.color ?? Colors.white,
     );
   }
 }
